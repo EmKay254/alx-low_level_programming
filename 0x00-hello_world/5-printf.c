17fb653084bca010b2344ad000c64e9fc1667230
EmKay254 <movinkiplrotich@gmail.com>
2022-12-12 02:49:07 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
- * main-main block
- * retun: 0
- * Description: A C program that prints with printf function
+ * main - main block
+ * Retun: 0
  */
-
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
