e3f0419d589fd4d2677de2f3302162ead978abc1
EmKay254 <movinkiplrotich@gmail.com>
2022-12-13 03:02:06 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main-entry point to the program
+ * Return: program terminates here
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 16; num++)
+	{
+		putchar('%x', num);
+	}
+	putchar('\n');
+	return (0);
+}
