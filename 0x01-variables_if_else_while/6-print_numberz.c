831c2cf1b8443ef09c75620c90eed5d3b1cf32e5
EmKay254 <movinkiplrotich@gmail.com>
2022-12-13 01:54:14 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main-main entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a < '10'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
