d1c82eb094099dec66ec6881ec9ea8692b762016
EmKay254 <movinkiplrotich@gmail.com>
2022-12-15 01:40:53 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,19 +12,19 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		_putchar(',');
-		_putchar('1');
+		_putchar(1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
 		_putchar(',');
-		_putchar('-1');
+		_putchar(-1);
 	}
 	else
 	{
 		_putchar('0');
 		_putchar(',');
-		_putchar('0');
+		_putchar(0);
 	}
 	return (0);
 }
