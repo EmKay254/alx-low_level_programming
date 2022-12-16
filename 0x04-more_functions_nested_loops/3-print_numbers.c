87626ca08b93bc39b4cbf4eb2a8edea00bc94059
EmKay254 <movinkiplrotich@gmail.com>
2022-12-16 05:14:48 -0800

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 1 through 9
+ * Return: Always success
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	return (0);
+}
