721a08eab1056059d342de329c34f5c2952dab3c
EmKay254 <movinkiplrotich@gmail.com>
2022-12-16 04:52:57 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,17 +7,9 @@
  */
 int _isupper(int c)
 {
-	char upper
-
-	for (upper = 'A'; upper <= 'Z'; upper++)
+	if ((c >= 'A') && (c <= 'Z'))
 	{
-		if (c == upper)
-		{
-			return (1);
-		}
-		else
-		{
-			return (0);
-		}
+		return (1);
 	}
+	return (0);
 }
