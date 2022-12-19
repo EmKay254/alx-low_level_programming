af192af5a16f6d3a574a94e5c9a82c5717bf2f94
EmKay254 <movinkiplrotich@gmail.com>
2022-12-19 04:11:24 -0800

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - resets the number to 98
+ *
+ * @*n: integer to reset
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
